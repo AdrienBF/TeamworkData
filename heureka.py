e2d0938a96bc429cc053fad067e49a9d994b3166
Matej Maivald <matej.maivald@outlook.com>
2019-05-14 20:19:51 +0200

    heureka.py: downloading information from heureka.cz, req.py: RequestHelper class to simplify requests

---
+++ heureka.py
@@ -0,0 +1,130 @@
+from urllib.parse import urljoin
+from concurrent.futures import ThreadPoolExecutor
+import re
+
+from req import RequestHelper
+
+
+class Heureka:
+    """
+    Downloads the data from the Heureka from the obchody.heureka.cz page.
+    """
+
+    def __init__(self, req=None, *, use_multiple_threads=False):
+        """
+        __init__ method for the class.
+
+        :param RequestHelper req: Request helper objects, if not supplied, instantiated automatically.
+        :param use_multiple_threads: If True, multiple threads are used. Handle with care.
+        """
+
+        self.r = req or RequestHelper()
+        # if not isinstance(req, RequestHelper):
+        #     raise NotImplementedError('Only RequestHelper instance can be passed in req argument')
+
+        self._next_link = None
+        self._output_from_list_page = []
+        self._use_multiple_threads = use_multiple_threads
+
+        self.heureka_output = []
+
+    def _download_list_of_links(self, how_many_pages_download=2):
+        """
+        Downloads the basic info available from the listing pages.
+
+        :param int how_many_pages_download: How many listings pages to open. One how_many_pages_download equals to
+                                            20 eshops downloaded.
+        """
+        # either continue where left or start at the beginning
+        self._next_link = self._next_link or 'https://obchody.heureka.cz/'
+        for _ in range(how_many_pages_download):
+            print(f'Getting link: {self._next_link}')
+            sel = self.r.get_selector(self._next_link)
+
+            # parse info from the listing page
+            self._parse_listing_page(sel)
+
+            # get the link for the "next" page
+            self._next_link = urljoin(self._next_link,
+                                      sel.xpath('/html/body/div[2]/div/div[2]/nav/ol//a[@rel="next"]/@href').get())
+
+    def _parse_listing_page(self, sel):
+        """
+        Parses the values from the listing page.
+
+        :param parsel.Selector sel: Input Selector object.
+        """
+        self._output_from_list_page.extend(
+            [{
+                # re.sub -> replace all that does not match digit
+                'reviews': float(re.sub(r'[^\d]+', '', tr.xpath('./td[4]/a/ul/li[2]/text()').get())),
+                # urljoin -> solves relative vs. absolute links
+                'inner_link': urljoin(self._next_link, tr.xpath('./td[4]/a/@href').get()),
+                'name': tr.xpath('./th/a/text()').get(),
+            }
+                for tr in sel.xpath('/html/body/div[2]/div/div[2]/div/table//tr')])
+
+    def _extend_list_page_by_details(self):
+        """
+        For the _output_from_list_page dictionary, gets the fields from the detailed page and
+        saves it to the heureka_output dictionary.
+        """
+        for eshop in self._output_from_list_page:
+            self.heureka_output.append(self._download_eshop_detail(eshop))
+
+    def _extend_list_page_by_details_quick(self, threads=10):
+        """
+        The same functionality as _extend_list_page_by_details method, but uses threads. Use with caution, otherwise,
+        you might get blocked.
+
+        :param int threads: Number of connections to be opened in parallel threads.
+        """
+        self.heureka_output = list(
+            ThreadPoolExecutor(threads).map(self._download_eshop_detail, self._output_from_list_page))
+
+    def _download_eshop_detail(self, eshop):
+        """
+        Wrapper for downloading the detail page
+
+        :param dict eshop: Dictionary, must contain the 'inner_link' key.
+        :return dict: Dictionary enhanced by the values from the detail page.
+        """
+        sel = self.r.get_selector(eshop['inner_link'])
+        eshop.update(self._parse_detail_page(sel))
+        return eshop
+
+    def _parse_detail_page(self, sel):
+        """
+        Parses the detail page
+
+        :param parsel.Selector sel: Input selector object.
+        :return dict: Returns the detailed values from the page.
+        """
+        output = {}
+        link = sel.xpath('/html/body/div[2]/div/'
+                         'div[2]/aside//dd[@class="c-pair-list__value"]/'
+                         'a[contains(@href,"heureka.cz/exit")]/text()').get().strip()
+        output['link'] = link
+
+        rating = sel.css('body > div.scope-essentials.scope-shop-detail > '
+                         'div > div.l-shop-detail__wrapper > aside > div > '
+                         'section.c-shop-detail-stats.c-aside__section > table > '
+                         'tbody > tr:nth-child(1) > td > '
+                         'span.c-shop-detail-stats__value::text').get()
+        output['rating'] = float(rating.replace(',', '.'))
+
+        return output
+
+    def run(self, how_many_pages_download):
+        """
+        Main method for the class, output is stored in the heureka_output attribute.
+
+        :param int how_many_pages_download: How many pages should be there in the output.
+        """
+        # there are 20 pages for
+        listings_needed = (how_many_pages_download // 20) + 1
+        self._download_list_of_links(listings_needed)
+        if self._use_multiple_threads:
+            self._extend_list_page_by_details_quick()
+        else:
+            self._extend_list_page_by_details()
---
+++ req.py
@@ -0,0 +1,68 @@
+import requests
+import random
+import time
+
+from parsel import Selector
+
+
+class RequestHelper:
+    """
+    This class attempts to enable to make requests more easily.
+    """
+
+    def __init__(self, timeout=10, maximum_retries=5, verify=False, proxy_list=None):
+        """
+        __init__ method for RequestHelper class
+
+        :param int timeout: Maximum wait time.
+        :param int maximum_retries: Maximum retries on URL opening.
+        :param bool verify: If set to false, skips https verification.
+        :param list proxy_list: List of strings of proxies to be used for opening urls.
+        """
+        self.proxy_list = proxy_list
+        self.timeout = timeout
+        self.maximum_retries = maximum_retries
+        self.verify = verify
+
+    def make_request(self, url):
+        """
+        Making requests with retry logic and headers mocking real-world browser
+
+        :param str url: Url to open
+        :return requests.Response: Response object
+        """
+        headers = {
+            'cache-control': 'max-age=0',
+            'upgrade-insecure-requests': '1',
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 '
+                          '(KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36',
+            'dnt': '1',
+            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,'
+                      'image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
+            'accept-encoding': 'gzip, deflate',
+            'accept-language': 'en,cs;q=0.9,sk;q=0.8,en-GB;q=0.7,en-US;q=0.6',
+        }
+
+        for attempt in range(self.maximum_retries):
+            try:
+                proxy_dict = None
+                if self.proxy_list:
+                    proxy_host = random.choice(self.proxy_list)
+                    proxy_dict = {'http': proxy_host, 'https': proxy_host}
+                print(f'Attempt {attempt} for {url}.')
+                r = requests.get(url, headers=headers, timeout=self.timeout, verify=self.verify, proxies=proxy_dict)
+                return r
+            except Exception as e:
+                if attempt < self.maximum_retries - 1:
+                    time.sleep(10)
+                    continue
+                raise
+
+    def get_selector(self, url):
+        """
+        Returns selector object for given url
+        :param str url: url to open
+        :return parsel.Selector: Selector object
+        """
+        r = self.make_request(url=url)
+        return Selector(r.text)
